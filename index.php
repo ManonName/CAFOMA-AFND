b2c95ad5041108dc21650a3c37858c84ebf185e7
Manon NAAMANE <mn.naamane@gmail.com>
2023-01-19 16:11:38 +0100

    Ceci est le premier commit du projet

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Bonjour, je suis le nouveau projet nommé 'ANFD' ! "
+        ?>
+    </body>
+</html>
